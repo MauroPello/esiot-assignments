c581b96c1ab1c7052182c9b938303fb07249aac0
Spezialissimo <davide.speziali@proton.me>
2023-11-21 11:33:30 +0100

    Fix in macros

--- Second assignment/src/arduino/lib/UserLCD/UserLCD.cpp
+++ Second assignment/src/arduino/lib/UserLCD/UserLCD.cpp
@@ -1,12 +1,12 @@
 #include <UserLCD.hpp>
-#define MaxCols 20
-#define MaxRows 4
+#define MAX_COLS 20
+#define MAX_ROWS 4
+#define LCD_ADDR 0x27
 
-UserLCD::UserLCD() : lcd{LiquidCrystal_I2C(0x27, MaxCols, MaxRows)} {
+UserLCD::UserLCD() : lcd{LiquidCrystal_I2C(LCD_ADDR, MAX_COLS, MAX_ROWS)} {
     this->lcd.init();
     this->lcd.backlight();
-    for (int i = 0; i < sizeof(barFragment) / sizeof(barFragment[0]); i++)
-    {
+    for (int i = 0; i < sizeof(barFragment) / sizeof(barFragment[0]); i++) {
         barFragment[i] = B11111;
     }
     lcd.createChar(0, barFragment);
@@ -17,8 +17,7 @@ void UserLCD::resetCursor() {
 }
 
 void UserLCD::printColumnProgress(int colIndex) {
-    for (size_t i = 0; i < 3; i++)
-    {
+    for (size_t i = 0; i < MAX_ROWS - 1; i++) {
         this->lcd.setCursor(i, colIndex);
         this->lcd.write(byte(0));
     }
@@ -27,7 +26,7 @@ void UserLCD::printColumnProgress(int colIndex) {
 void UserLCD::drawProgressBar(int progress, int max) {
     this->resetCursor();
     this->lcd.clear();
-    int barProgress = (progress * 20) / max;
+    int barProgress = (progress * MAX_COLS) / max;
     for (int i = 0; i < barProgress; i++) {
         this->printColumnProgress(i);
     }
@@ -38,15 +37,14 @@ void UserLCD::print(String msg) {
     int i = 0;
     int cursorLine = 0;
     int cursorCol = 0;
-    while (msg[i] != '\n')
-    {
+    while (msg[i] != '\n') {
         this->lcd.print(msg[i]);
         this->lcd.setCursor(cursorLine, cursorCol);
         cursorCol++;
-        if (cursorCol == 19)
+        if (cursorCol == MAX_COLS - 1)
         {
             cursorLine++;
-            cursorCol=0;
+            cursorCol = 0;
         }
         i++;
     }
