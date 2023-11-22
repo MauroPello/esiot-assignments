47bec0ce5e032f323794bd5a2fa9aca4ad4d31da
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-22 09:47:55 +0100

    Temp FIx

--- Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.cpp
+++ Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include <TemperatureSensor.hpp>
+#define SENSOR_VOLTAGE 3.3
+#define SENSOR_RESOLUTION 1024
+#define CONVERSION_FACTOR SENSOR_VOLTAGE / SENSOR_RESOLUTION
 
 TemperatureSensor::TemperatureSensor(int pin) : pin{pin} {
     pinMode(pin, INPUT);
 }
 
 float TemperatureSensor::read() {
-    return analogRead(pin) * 0.48828125;
+    return analogRead(pin) * CONVERSION_FACTOR;
 }
