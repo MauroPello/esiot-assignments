2167342e401affe0db53973f486c2f49094d9c46
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-22 09:53:20 +0100

    Temp fix 2

--- Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.cpp
+++ Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.cpp
@@ -5,9 +5,17 @@
 #define CONVERSION_FACTOR SENSOR_VOLTAGE / SENSOR_RESOLUTION
 
 TemperatureSensor::TemperatureSensor(int pin) : pin{pin} {
+    analogReference(EXTERNAL);
     pinMode(pin, INPUT);
 }
 
 float TemperatureSensor::read() {
-    return analogRead(pin) * CONVERSION_FACTOR;
+    int val_Adc = 0;
+    for(byte Ciclo = 0; Ciclo < 100; Ciclo++) {
+        val_Adc += analogRead(pin);
+        delay(10);
+    }
+    val_Adc /= 100;
+    temp = ((val_Adc * CONVERSION_FACTOR) - 0.5) / 0.01;
+    return temp;
 }
