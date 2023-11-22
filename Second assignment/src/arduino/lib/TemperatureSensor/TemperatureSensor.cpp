e63c2dab1b267573e7f62c2dd2586731bd880db6
Spezialissimo <davide.speziali@proton.me>
2023-11-22 13:46:19 +0100

    improve temperature measurements

--- Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.cpp
+++ Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.cpp
@@ -1,16 +1,22 @@
-#include <Arduino.h>
 #include <TemperatureSensor.hpp>
+#include <Arduino.h>
+
+#define MAX_MEASUREMENTS 10
 #define SENSOR_VOLTAGE 3.3
 #define SENSOR_RESOLUTION 1024.0
 #define CONVERSION_FACTOR SENSOR_VOLTAGE / SENSOR_RESOLUTION
 
-TemperatureSensor::TemperatureSensor(int pin) : pin{pin} {
+TemperatureSensor::TemperatureSensor(int pin) : pin{pin}, measurementsSum{0}, measurements{MAX_MEASUREMENTS} {
     pinMode(pin, INPUT);
 }
 
 float TemperatureSensor::read() {
-    float vout = analogRead(pin) * CONVERSION_FACTOR;
-    // Serial.println("V: " + String(analogRead(pin)));
-    // Serial.println("T: " + String((vout - 0.5) * 100));
-    return (vout - 0.5) * 100;
+    float temperature = ((analogRead(pin) * CONVERSION_FACTOR) - 0.5) * 100;
+
+    if (this->measurements.isFull()) {
+        measurementsSum -= this->measurements.dequeue();
+    }
+    this->measurements.enqueue(temperature);
+    measurementsSum += temperature;
+    return measurementsSum / this->measurements.itemCount();
 }
--- Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.hpp
+++ Second assignment/src/arduino/lib/TemperatureSensor/TemperatureSensor.hpp
@@ -1,9 +1,12 @@
 #ifndef TEMPERATURE_SENSOR_HPP
 #define TEMPERATURE_SENSOR_HPP
+#include <ArduinoQueue.h>
 
 class TemperatureSensor {
     private:
         const int pin;
+        float measurementsSum;
+        ArduinoQueue<float> measurements;
     public:
         TemperatureSensor(int pin);
         float read();
