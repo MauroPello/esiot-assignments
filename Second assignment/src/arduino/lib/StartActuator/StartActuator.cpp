05c7bda41c8c9c7af31a2ed26dd3435fb472e183
Spezialissimo <davide.speziali@proton.me>
2023-11-23 10:53:48 +0100

    make code shorter

--- Second assignment/src/arduino/lib/StartActuator/StartActuator.cpp
+++ Second assignment/src/arduino/lib/StartActuator/StartActuator.cpp
@@ -4,9 +4,5 @@
 StartActuator::StartActuator(int pin) : pin{pin} { pinMode(pin, INPUT); }
 
 bool StartActuator::isActive() {
-    if (digitalRead(pin) == HIGH) {
-        return true;
-    } else {
-        return false;
-    }
+    return digitalRead(pin) == HIGH;
 }
