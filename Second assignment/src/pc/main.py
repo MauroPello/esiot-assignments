49f42ee5a7bf9957e910b0a75a13fbe83b16e66b
Spezialissimo <davide.speziali@proton.me>
2023-11-24 00:50:45 +0100

    Add support for more OS

--- Second assignment/src/pc/main.py
+++ Second assignment/src/pc/main.py
@@ -1,6 +1,8 @@
 from tkinter import ttk
 import tkinter as tk
 import serial
+import sys
+import glob
 
 def update_serial_data():
     """Updates the data from the serial"""
@@ -45,9 +47,37 @@ def maintenance_done():
         continue
     arduino.write("SOLVED".encode())
 
+def serial_ports():
+    """ Lists serial port names
 
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
-arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+ports=serial_ports()
+
+arduino = serial.Serial(port=ports[0], baudrate=9600)
 
 
 root = tk.Tk()
