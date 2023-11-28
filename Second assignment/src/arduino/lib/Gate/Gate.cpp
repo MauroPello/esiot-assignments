9cd33c0bd63dc66832bf7084a3c1a191c3a1b35b
Spezialissimo <davide.speziali@proton.me>
2023-11-28 01:53:15 +0100

    Fix motor orientation

--- Second assignment/src/arduino/lib/Gate/Gate.cpp
+++ Second assignment/src/arduino/lib/Gate/Gate.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <Gate.hpp>
 
-#define DEGREES_GATE_CLOSED 0
-#define DEGREES_GATE_OPEN 97
+#define DEGREES_GATE_CLOSED 97
+#define DEGREES_GATE_OPEN 0
 
 Gate::Gate(int pin) : pin{pin}, servo{} {
     this->servo.attach(pin);
