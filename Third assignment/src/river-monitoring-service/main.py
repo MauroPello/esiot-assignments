799459a559ea9f75486168c569f0dc1366058843
Mauro Pellonara <mauro.pellonara@studio.unibo.it>
2024-02-02 22:27:52 +0100

    finish multiple dashboards support

--- Third assignment/src/river-monitoring-service/main.py
+++ Third assignment/src/river-monitoring-service/main.py
@@ -9,18 +9,17 @@ import sys
 app = Flask(__name__)
 water_channel_controller = None
 mqtt_client = mqtt.Client()
+dashboards = []
 
 with open(sys.argv[0].replace("main.py", "") + "config.json") as config_file:
     config_json = json.load(config_file)
     backend_ip = config_json["backend_ip"]
     backend_port = config_json["backend_port"]
-    frontend_url = config_json["frontend_url"]
     broker_ip = config_json["broker_ip"]
     broker_port = config_json["broker_port"]
     send_topic = config_json["send_topic"]
     receive_topic = config_json["receive_topic"]
     water_channel_controller_port = config_json["water_channel_controller_port"]
-    frontend_url = config_json["frontend_url"]
     WL1 = config_json["WL1"]
     WL2 = config_json["WL2"]
     WL3 = config_json["WL3"]
@@ -30,6 +29,24 @@ with open(sys.argv[0].replace("main.py", "") + "config.json") as config_file:
     system_state = config_json["initial_system_state"]
     valve_percentage = config_json["initial_valve_percentage"]
     valve_state = config_json["initial_valve_state"]
+
+
+def update_dashboards(endpoint, params):
+    for dashboard in dashboards:
+        try:
+            requests.post(dashboard + endpoint, json=params, timeout=2)
+        except:
+            dashboards.remove(dashboard)
+
+
+@app.route('/registerNewDashboard', methods=['POST'])
+def register_new_dashboard():
+    dashboard_addr = "http://" + request.remote_addr + ":" + request.environ["REMOTE_PORT"] + "/"
+    if dashboard_addr in dashboards:
+        return jsonify({'error': 'Dashboard già registrata!.'}), 400
+    else:
+        dashboards.append(dashboard_addr)
+        return jsonify({'message': 'Dashboard registrata con successo.'}), 200
 
 
 @app.route('/getWaterLevelThresholds', methods=['GET'])
@@ -73,15 +90,14 @@ def check_water_channel_controller():
                 old_valve_percentage = valve_percentage
                 valve_percentage = data[0].split(": ")[1]
                 if old_valve_percentage != valve_percentage:
-                    params = {'newPercentage': valve_percentage}
-                    requests.post(frontend_url + 'setNewValvePercentageFromSystem', json=params)
+                    update_dashboards('setNewValvePercentageFromSystem', {'newPercentage': valve_percentage})
 
                 global valve_state
                 old_valve_state = valve_state
                 valve_state = data[1].split(": ")[1].replace(";", "")
                 if old_valve_state != valve_state:
                     params = {'newState': "false" if valve_state == "MANUAL" else "true"}
-                    requests.post(frontend_url + 'setValveUpdatabilityState', json=params)
+                    update_dashboards('setValveUpdatabilityState', params)
         except:
             pass
 
@@ -92,40 +108,39 @@ def on_message(client, userdata, msg):
         if data[0] == "WL":
             global system_state
             water_level = float(data[1])
-            params = {'level': water_level}
-            requests.post(frontend_url + 'setLatestWaterLevel', json=params)
+            update_dashboards('setLatestWaterLevel', {'level': water_level})
             if water_level < WL1:
                 old_system_state = system_state
                 system_state = "ALARM-TOO-LOW"
                 if old_system_state != system_state:
-                    requests.post(frontend_url + 'setSystemState', json={'newState': system_state})
+                    update_dashboards('setSystemState', {'newState': system_state})
                     set_water_channel_valve_percentage(0)
             elif water_level <= WL2:
                 old_system_state = system_state
                 system_state = "NORMAL"
                 if old_system_state != system_state:
-                    requests.post(frontend_url + 'setSystemState', json={'newState': system_state})
+                    update_dashboards('setSystemState', {'newState': system_state})
                     set_water_channel_valve_percentage(25)
                     set_water_level_measurement_freq(F1)
             elif water_level <= WL3:
                 old_system_state = system_state
                 system_state = "PRE-ALARM-TOO-HIGH"
                 if old_system_state != system_state:
-                    requests.post(frontend_url + 'setSystemState', json={'newState': system_state})
+                    update_dashboards('setSystemState', {'newState': system_state})
                     set_water_channel_valve_percentage(25)
                     set_water_level_measurement_freq(F2)
             elif water_level <= WL4:
                 old_system_state = system_state
                 system_state = "ALARM-TOO-HIGH"
                 if old_system_state != system_state:
-                    requests.post(frontend_url + 'setSystemState', json={'newState': system_state})
+                    update_dashboards('setSystemState', {'newState': system_state})
                     set_water_channel_valve_percentage(50)
                     set_water_level_measurement_freq(F2)
             else:
                 old_system_state = system_state
                 system_state = "ALARM-TOO-HIGH-CRITIC"
                 if old_system_state != system_state:
-                    requests.post(frontend_url + 'setSystemState', json={'newState': system_state})
+                    update_dashboards('setSystemState', {'newState': system_state})
                     set_water_channel_valve_percentage(100)
                     set_water_level_measurement_freq(F2)
 
